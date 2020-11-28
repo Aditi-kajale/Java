132c2e724c8b22180e511c46271db09cb79e93d6
Aditi Kajale <55836122+Aditi-kajale@users.noreply.github.com>
2020-11-28 10:40:18 +0530

    Add files via upload

---
+++ Prog11.java
@@ -0,0 +1,8 @@
+class Prog11
+{
+	public static void main(String args[])
+	{	//  float f1=9.0;   error loss of precision
+		float f2=(float)9.0;
+		float f3=9.0F;    //or f
+	}
+}
---
+++ Prog12.java
@@ -0,0 +1,9 @@
+class Prog12
+{
+	public static void main(String args[])
+	{	byte b1=100;
+		int i =100;
+		// byte b2=i;         assigning to a cons and var are diff
+		byte b2=(byte)i;
+	}
+}
